cdfca14a81e97f0a66c773df6685e321350e5d41
dreamhandg <handg0972@gmail.com>
2022-09-04 20:27:19 +0900

    first commit

---
+++ src/main/java/com/batch/edu/SpringBatchEduApplication.java
@@ -0,0 +1,13 @@
+package com.batch.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/edu/SpringBatchEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
